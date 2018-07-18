1c1981a33bbf98da7dcf49ad05988e27c5aa3849
SimiganoschiMatei <intern.matei.simiganoschi@assist.ro>
2018-07-18 18:05:06 +0300

    login&createInterfaces

--- src/app/components/shared/login/login.component.ts
+++ src/app/components/shared/login/login.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+ 
   constructor() { }
+
+isDisplayed: boolean = false;
+  
 
   ngOnInit() {
   }
