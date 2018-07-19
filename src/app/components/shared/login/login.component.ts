43a89ed9fa06d139ea4d6bdd825f728903be4d08
SerediucM <intern.mihaela.serediuc@assist.ro>
2018-07-19 19:09:23 +0300

    add links between login pages

--- src/app/components/shared/login/login.component.ts
+++ src/app/components/shared/login/login.component.ts
@@ -11,8 +11,13 @@ import {ApiConnectionService} from '../../../services/api-connection/api-connect
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- 
+  
 
+  state1:string="hide";
+  state2:string="show";
+  state3:string="hide";
+  state4:string="hide";
+  state5:string="hide";
 
    @Input() users: User;
    isDisplayed: boolean = false;
@@ -24,23 +29,13 @@ export class LoginComponent implements OnInit {
   
   ngOnInit():void {
     this.getUsers();
-
-
   }
-  //  getUsers(): void {
-  //   this.userConn.getUsers().subscribe(data => this.users = data);
-  //  }
   loginUser(e){
     e.preventDefault();
     console.log(e);
      var username = e.target.elements[0].value;
      var password = e.target.elements[1].value;
      this.getUsers().subscribe(data => {
-    //  let res =  data;
-    //  res.map(item => {
-    //   console.log(item);
-    //  });
-    //  console.log(data)
     data.forEach(item => {
        if(username == item.email && password == item.password){
         console.log(username,password, "ok")
@@ -49,7 +44,41 @@ export class LoginComponent implements OnInit {
    });
     });
   }
-  
+  // SingUp
+  createCont(){ 
+    this.state1 = "show";
+    this.state2 = "hide";
+    this.state3 = "hide";
+    this.state4 = "hide";
+  }
+  // CREATE ACCOUNT
+  SpreLogin(){
+    this.state1="hide";
+    this.state2 ="show";
+    this.state3 = "hide";
+    this.state4 = "hide";
+  }
+  // Reset it->Email
+  ResetEmail(){
+    this.state1="hide";
+    this.state2 ="hide";
+    this.state3 = "show";
+    this.state4 = "hide";
+  }
+  //reset Email-pass
+  ResetPass(){
+    this.state1="hide";
+    this.state2 ="hide";
+    this.state3 = "hide";
+    this.state4 = "show";
+  }
+    //reset pass-login
+    ResetComplet(){
+      this.state1="hide";
+      this.state2 ="show";
+      this.state3 = "hide";
+      this.state4 = "hide";
+    }
   getUsers()  {
     const id = +this.router.snapshot.paramMap.get('id');
     return this.userConn.getUser(id)
