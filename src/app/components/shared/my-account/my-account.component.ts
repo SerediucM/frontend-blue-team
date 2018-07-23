25b894e9aebe8f63979f61a74f4d2929f2fefa7d
Marian Gheorghiu <marian_gg@yahoo.com>
2018-07-23 16:38:10 +0300

    myAccountV2

--- src/app/components/shared/my-account/my-account.component.ts
+++ src/app/components/shared/my-account/my-account.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-// import {Router, ActivatedRoute } from '@angular/router';
-// import {User} from '../../../shared/user interface/user';
-// import {Location} from '@angular/common';
-// import {ApiConnectionService} from '../../../services/api-connection/api-connection.service';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import {Router, ActivatedRoute } from '@angular/router';
+import {User} from '../../../shared/user interface/user';
+import {Location} from '@angular/common';
+import {ApiConnectionService} from '../../../services/api-connection/api-connection.service';
+import { HttpClient } from '@angular/common/http'; 
+
+
 
 
 
@@ -11,30 +14,48 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './my-account.component.html',
   styleUrls: ['./my-account.component.css']
 })
-export class MyAccountComponent implements OnInit {
+export class MyAccountComponent implements OnInit, AfterViewInit {
+
+
+ @Input() users: User;
+
+private loggedUserName: string;
+private loggedUserPass: string;
+private loggedUserEmail: string;
  
+  isDisplayed: boolean = true;
+ constructor(private router: ActivatedRoute ,
+             private rout:Router,
+             private location: Location,
+             private userConn: ApiConnectionService,
+             private http: HttpClient
+             
+ ) { 
 
-  constructor(
-    // private router: ActivatedRoute ,
-    //          private rout:Router,
-    //          private location: Location,
-    //          private userConn: ApiConnectionService
+     
+ }
 
-  ) { }
+ ngAfterViewInit():void {
+  this.getUsers().subscribe(data => {
+      this.loggedUserName = data[0].name;
+      this.loggedUserPass = data[0].password;
+      this.loggedUserEmail = data[0].email;   
+  });
+ }
 
   ngOnInit():void {
-    // this.getUsers();
+    this.getUsers();
   }
 
-  // loginUser(e){
-  //   e.preventDefault();
-  //   console.log(e);
-  //   this.getUsers().subscribe(data => {
-  //   data.forEach(item => {
-       
-  //  });
-  //   });
+  loginUser(e){
+    e.preventDefault();
+    console.log(e);
 
 
-
+    
+}
+getUsers(){
+  const id = +this.router.snapshot.paramMap.get('id');
+  return this.userConn.getUser(id)
+}
 }
